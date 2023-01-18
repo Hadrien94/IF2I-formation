6df9256186b12038f7ff87900d1fd2195b39d0bb
Poulenc <hpoulenc@gmail.com>
2023-01-18 14:35:59 +0100

    modif laure

--- Test.java
+++ Test.java
@@ -3,8 +3,20 @@ package fil_rouge;
 public class Test {
 
 	public static void main(String[] args) {
-
 		
+		//pour tester
+		ArrayList<Activite> activites = null;
+		
+		Activite activite = new Activite();///
+		//liste avec unique élément
+		activites.add(activite);
+		
+		ActiviteService activiteService = new ActiviteService();
+		
+		
+		System.out.println(activiteService.listeActivite().get(0).toString());
+
+	   
 		
 	}
 
---
+++ com/formation/service/ActiviteService.java
@@ -0,0 +1,22 @@
+
+public class ActiviteService {
+	
+	public ArrayList<Activite> listeActivite(){
+		
+		ArrayList<Activite> activites = null;
+		
+		//algo
+		
+		return activites;
+		
+	}
+	
+    public ArrayList<Activite> listeActiviteReserve(){
+    	
+        ArrayList<Activite> activites = null;
+		
+		return activites;
+		
+	}
+
+}
